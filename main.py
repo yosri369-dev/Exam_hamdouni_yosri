b965363d78a5e159a782525d181c74c80abd9773
yosri <hamdouni.yosri@gmail.com>
2025-03-14 09:47:27 +0100

    commit Q16

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 my_list=[1,5,7]
-print ("la somme est :", sum(my_list))
+if my_list:
+        print ("la somme est :", sum(my_list))
+        print("le max est : ", max(my_list))
+        print("le min est : ", min(my_list))
+else :
+        print("liste vide")
 
